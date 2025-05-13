d179773ea921a03b8d726ac73ef3026da74bbbe5
Kostya-X <constantine-x@outlook.com>
2025-05-13 19:57:16 +0300

    Pizdulina 4ever

---
+++ content.js
@@ -0,0 +1,24 @@
+function replaceText(node) {
+  if (node.nodeType === Node.TEXT_NODE) {
+    node.textContent = node.textContent.replace(/Мизулина/g, 'Пиздулина');
+  } else {
+    for (let i = 0; i < node.childNodes.length; i++) {
+      replaceText(node.childNodes[i]);
+    }
+  }
+}
+
+replaceText(document.body);
+
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    mutation.addedNodes.forEach((node) => {
+      replaceText(node);
+    });
+  });
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});

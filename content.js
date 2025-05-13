c62dee3ee778e7a824628aba158ac48b231742f3
Kostya-X <constantine-x@outlook.com>
2025-05-13 20:28:57 +0300

    make text replacemnt case-insesitive

--- content.js
+++ content.js
@@ -1,6 +1,9 @@
 function replaceText(node) {
   if (node.nodeType === Node.TEXT_NODE) {
-    node.textContent = node.textContent.replace(/Мизулина/g, 'Пиздулина').replace(/Мизулиной/g, 'Пиздулиной').replace(/Мизулину/g, 'Пиздулину');
+    node.textContent = node.textContent
+      .replace(/Мизулина/gi, 'Пиздулина')
+      .replace(/Мизулиной/gi, 'Пиздулиной')
+      .replace(/Мизулину/gi, 'Пиздулину');
   } else {
     for (let i = 0; i < node.childNodes.length; i++) {
       replaceText(node.childNodes[i]);
